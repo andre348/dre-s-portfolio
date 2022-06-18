e5770cca2142b16c6bfd554f47a380e9e94d5153
andre348 <andrejenkins22@yahoo.com>
2022-06-18 15:18:04 -0400

    update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,13 @@
 //Step 1: Import React
-import * as React from 'react'
-import * as React from "../components/layout"
-import { Link } from 'gatsby'
+import * as React from "react"
 
 //Step 2: Define your component
-const BlogPage = () => {
+const IndexPage = () => {
   return (
     <main>
-      <title>Andre Jenkins</title>
+      <title>My Personal Portfolio</title>
      <h1>Welcome to my website.</h1>
-     <p>This is the Blog Page.</p>
-     <Link to="/">Back to Home</Link>
+     <p>This is my homepage.</p>
      </main>
   )
 }
