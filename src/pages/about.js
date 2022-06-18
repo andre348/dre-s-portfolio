d2e57148e139173753c66e93ae837f219fb4a8bc
andre348 <andrejenkins22@yahoo.com>
2022-06-18 18:07:19 -0400

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,7 +9,7 @@ const AboutPage = () => {
             <title>Andre Jenkins</title>
             <h1>About Me</h1>
             <p>Andre Jenkins is from Columbia, South Carolina and is an undergrad student at the University of South Carolina. He is majoring in Information Science and intends to pursue am M.A. in Data Science. Andre is currently 24 years old.</p>
-<Link to= "/" >Home</Link>
+            <Link to= "/" >Home</Link>
         </main>
     ) 
 }    
