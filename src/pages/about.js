30c2da33672bbbf5c58f331121b2419651f4c022
andre348 <andrejenkins22@yahoo.com>
2022-06-18 17:56:02 -0400

    update

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,6 @@
 //Step 1: Import React
 import * as React from 'react'
+import { Link } from 'gatsby'
 
 // Step 2: Define your component
 const AboutPage = () => {
@@ -8,6 +9,7 @@ const AboutPage = () => {
             <title>Andre Jenkins</title>
             <h1>About Me</h1>
             <p>Andre Jenkins is from Columbia, South Carolina and is an undergrad student at the University of South Carolina. He is majoring in Information Science and intends to pursue am M.A. in Data Science. Andre is currently 24 years old.</p>
+<Link to= "/" >Home</Link>
         </main>
     ) 
 }    
