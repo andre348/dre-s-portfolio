4d554e69f82035c6b0cf8c4bdf2a318706043ebe
andre348 <andrejenkins22@yahoo.com>
2022-06-18 21:43:59 -0400

    update config.js

---
+++ gatsby-config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  siteMetadata: {
+    title: `Dre's-portfolio`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options:{
+        name: `pages`,
+        path: `${__dirname}/src/pages`
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options:{
+        name: `posts`,
+        path: `${__dirname}/src/posts`
+      },
+    },
+  {
+    resolve: `gatsby-source-filesystem`,
+    options:{
+      name: `images`,
+      path: `${__dirname}/src/images`
+    },
+  },
+  "gatsby-plugin-image",
+  "gatsby-plugin-sharp",
+  ]
+}
